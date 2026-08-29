2f3e2615f9bdb1d813f54ed84cbcc82c7029d3ab
Modforge <modforgerator@gmail.com>
2026-08-28 21:51:07 -0700

    Version 1.1.1

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,10 +3,10 @@ namespace BuyableInstaMonkeys;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "55.0";
-    public const string Version = "1.0.0";
+    public const string Version = "1.1.1";
     public const string Name = "Buyable Insta Monkeys";
 
-    public const string Description = "Buy Instas: Random 200 MM; 0-0-0 50 MM; Tier 1 100 MM; Tier 2 250 MM; Tier 3 500 MM; Tier 4 600 MM; Tier 5 1000 MM.";
+    public const string Description = "Buy Instas: Random 250 MM; 0-0-0 125 MM; Tier 1 250 MM; Tier 2 500 MM; Tier 3 1250 MM; Tier 4 2500 MM; Tier 5 5000 MM.";
 
     public const string RepoOwner = "modforgeyt";
     public const string RepoName = "BuyableInstaMonkeys";
